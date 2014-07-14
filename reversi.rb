07d0dc8738139ad437b832fd7026b3718485a2fb
Tom White <twar59@gmail.com>
2014-07-14 07:49:44 -0600

    start adding some pages

--- reversi.rb
+++ reversi.rb
@@ -1,6 +1,14 @@
 class Reversi < Sinatra::Application
   get '/' do
-    @board = Board.new
+    @name_of_game = "reversi"
+
     erb :index
+  end
+
+  get '/about' do
+    erb :about
+  end
+
+  get '/game' do
   end
 end
